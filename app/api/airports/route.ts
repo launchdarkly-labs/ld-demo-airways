57d5aebaa6003ee9bd9665b7716c46e5adc1214c
Henry Barrow <hbarrow@launchdarkly.com>
2024-03-27 14:29:28 +0000

    Add comments

--- app/api/airports/route.ts
+++ app/api/airports/route.ts
@@ -14,6 +14,7 @@ export async function GET(request: NextRequest, response: NextResponse) {
     const ldClient = await getServerClient();
     const context = getFlagContext();
 
+    // First check to see if we should use FastCache
     const enableFastCache = await ldClient.variation(
       "enableFastCache",
       context,
@@ -25,6 +26,7 @@ export async function GET(request: NextRequest, response: NextResponse) {
       return Response.json({ allAirports });
     }
 
+    // if flightDb is enabled, fetch data from Postgres. Otherwise, fetch from Redis.
     const flightDb = await ldClient.variation("flightDb", context, false);
     if (flightDb) {
       console.log("Fetching data from Postgres");
